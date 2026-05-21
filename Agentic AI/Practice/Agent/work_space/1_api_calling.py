6199d51a92a38220b79f2cf8fd28a8cd7685ede4
hengchhen <lyhengchhen99@gmail.com>
2026-05-21 21:46:54 +0700

    Add files via upload

---
+++ Agentic AI/Practice/Agent/work_space/1_api_calling.py
@@ -0,0 +1,20 @@
+from openai import OpenAI 
+import os
+
+client = OpenAI(api_key=os.getenv("API_KEY"))
+
+
+completion = client.chat.completions.create(
+    model="gpt-4o",
+    messages=[
+        {"role": "system", "content": "You're a helpful assistant."},
+        {
+            "role": "user",
+            "content": "Write a limerick about the Python programming language.",
+        },
+    ],
+)
+
+response = completion.choices[0].message.content
+print(response)
+
